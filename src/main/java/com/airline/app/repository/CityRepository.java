a77a5bafa993eb359a2826c5574d0a7da85dd2ab
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-02-18 09:07:46 -0500

    Correcciones en consultas a base de datos

--- src/main/java/com/airline/app/repository/CityRepository.java
+++ src/main/java/com/airline/app/repository/CityRepository.java
@@ -9,7 +9,6 @@ import com.airline.app.entity.City;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
-	// Consultar si la ciudad a la que se viaja necesita tener visa, por medio del nombre de la ciudad
 	@Query(value = "select visa from city where city.name=:name", nativeQuery = true)
 	String visaRequired(String name);
 }
--- src/main/java/com/airline/app/repository/RouteRepository.java
+++ src/main/java/com/airline/app/repository/RouteRepository.java
@@ -11,12 +11,10 @@ import com.airline.app.entity.Route;
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
 
-	// Listar las ciudades de origen
 	@Query(value = "select distinct name from city inner join route "
 			+ "on city.idCity = route.originCity;", nativeQuery = true)
 	List<String> originCitiesList();
 	
-	// Listar las ciudades destino de acuerdo a una ciudad origen
 	@Query(value = "select cDestination.name from city as cOrigin inner join route r "
 			+ "on cOrigin.idCity = r.originCity inner join city as cDestination "
 			+ "on r.destinationCity = cDestination.idCity where cOrigin.name=:originCity", 
